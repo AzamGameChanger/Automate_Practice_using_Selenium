60e472afaa5e7f91cb7e512b5c415aef05e7196d
Azamkhuja Ibragimov <ibragimov.azamkhuja.2020@gmail.com>
2021-02-10 14:51:33 -0500

    Actions class, window handles and etc.

---
+++ src/com/codewithazam/Day9/ChildWindow.java
@@ -0,0 +1,28 @@
+package com.codewithazam.Day9;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class ChildWindow {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/codewithazam/Downloads/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://demoqa.com/browser-windows");
+        driver.manage().window().maximize();
+
+        //driver.findElement(By.id("tabButton")).click();
+        driver.findElement(By.id("windowButton")).click();
+        Set<String> windowHandles= driver.getWindowHandles();
+        Iterator<String> it = windowHandles.iterator();
+        String parent = it.next();
+        String child = it.next();
+
+        driver.switchTo().window(child);
+        System.out.println(driver.findElement(By.cssSelector("h1[id='sampleHeading']")).getText());
+    }
+}
